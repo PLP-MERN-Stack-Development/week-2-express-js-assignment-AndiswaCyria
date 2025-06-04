edde53b4f113d7dfbc50838654753e2ac937cc8c
Andiswa Molangathi <andiswacyriam@gmail.com>
2025-06-04 22:05:24 +0200

    Delete middleware directory

--- middleware/logger.js
+++
@@ -1,6 +0,0 @@
-// middleware/logger.js
-module.exports = function logger(req, res, next) {
-  const now = new Date().toISOString();
-  console.log(`[${now}] ${req.method} ${req.originalUrl}`);
-  next();
-};
